ce13aa05aaaf90f655dcbb5c4d6d3ce8cd1d9917
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-03-04 16:55:11 +0530

    Modules

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
+const math = require("./math")
+const {add,sub, mult, mod} = require("./math");
+
 console.log("Hello World!");
+console.log(math);
+
+console.log(add(5,2))
+console.log(sub(5,2))
+console.log(mult(5,2))
+console.log(mod(5,2))
+
