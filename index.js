e9b2d9026fdd302aa05656067fbc17e5ea084c6d
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-03-06 01:04:56 +0530

    Implemented http status codes

--- index.js
+++ index.js
@@ -42,17 +42,21 @@ app.get("/api/users", (req, res) => {
 
 app.post("/api/users",(req, res) => {
     const body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.gender || !body.email || !body.job_title){
+        return res.status(400).send("Please enter all fields")
+    }
     body.id = users.length + 1;
     users.push(body);
     fs.writeFile("./mock.json", JSON.stringify(users), (err,data) => {
-        res.json(body)
+        res.status(201).json(body)
     })
 })
 
 app.route("/api/users/:id")
     .get((req, res) => {
         const id = Number(req.params.id)
-        res.json(users.find(user => user.id === id))
+        const user =  users.find(user => user.id === id)
+        user !== undefined ? res.json() : res.status(404).send("Not Found");
     })
     .patch((req, res) => {
         const id = Number(req.params.id)
@@ -61,14 +65,14 @@ app.route("/api/users/:id")
         let allUsersExcept = users.filter(user => user.id !== id);
         allUsersExcept.push(body);
         fs.writeFile("./mock.json", JSON.stringify(allUsersExcept), (err,data) => {
-            res.json(body)
+            res.status(203).json(body)
         })
     })
     .delete((req, res) => {
         const id = Number(req.params.id)
         let allUsersExcept = users.filter(user => user.id !== id);
         fs.writeFile("./mock.json", JSON.stringify(allUsersExcept), (err,data) => {
-            res.json({status: "success", users: allUsersExcept})
+            res.status(203).json({status: "success", users: allUsersExcept})
         })
     })
 
