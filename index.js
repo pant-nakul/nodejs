c20508ae2b5ff47a775a9af892cd757d415a5676
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-03-05 17:13:17 +0530

    Building HTTP server in NodeJs

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("STarting node server");
+console.log("Starting node server");
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+    const log = `${Date.now()} : ${new Date()} : ${req.url} : New Request Received \n`;
+    fs.appendFile("app.log", log, (err, result) => {
+
+        switch (req.url) {
+            case "/":
+                res.end("Home Page");
+                break;
+            case "/nakul":
+                res.end("I am Nakul");
+                break;
+            default:
+                res.end("404! Not Found");
+        }
+
+
+    })
+});
+
+server.listen("8000", () => {
+    console.log("Server is running!");
+})
+
