d362212057251354196d357ad0e7bfe7d4a23659
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-03-05 17:56:45 +0530

    Added url dependency, modified index.js

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ console.log("Starting node server");
 
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
     const log = `${Date.now()} : ${new Date()} : ${req.url} : New Request Received \n`;
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
     fs.appendFile("app.log", log, (err, result) => {
 
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case "/":
                 res.end("Home Page");
                 break;
-            case "/nakul/ashish":
-                res.end("I am Nakul Ashish Pant");
+            case "/about":
+                const username = myUrl.query?.name || "Ashish";
+                res.end(`My Name is ${username}`);
                 break;
             default:
                 res.end("404! Not Found");
