819a8d655634e922b4caedcad1a3f9fa4dd50652
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-03-05 21:04:37 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 console.log("Starting node server");
 
-const http = require("http");
 const express = require("express");
 
 const app = express();
+
+const port = 8000;
 
 app.get("/", (req, res) => {
     res.send("Hello from Express Homepage")
@@ -13,9 +14,7 @@ app.get("/about", (req, res) => {
     res.send(`Hello ${req.query?.name || "Nakul"} , your age is ${req.query.age || 21}`)
 })
 
-const server = http.createServer(app);
-
-server.listen("8000", () => {
-    console.log("Server is running!");
+app.listen(port, () => {
+    console.log("Server is running on port - " + port);
 })
 
