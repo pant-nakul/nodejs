7cf1064f3d8fbffca67f464f4e24756586619c75
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-03-06 01:55:26 +0530

    Added comment to routes loading file

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const router = express.Router();
 
-// Read all files in the routes directory
+// Read all files in the routes directory, routes will be created by filename.js. E.g. users.js will result in route /users
 fs.readdirSync(__dirname).forEach((file) => {
     if (file !== 'index.js' && file.endsWith('.js')) {
         const route = require(path.join(__dirname, file));
