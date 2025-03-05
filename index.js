787bd005fea87d839cc602cf3b9e90034add1273
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-03-05 20:21:28 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ const fs = require("fs");
 const url = require("url");
 
 const server = http.createServer((req, res) => {
-    console.log(req)
-    const myUrl = url.parse(req.url,true);
+    const parsedUrl = url.parse(req.url,true);
     const log = `${Date.now()} : : ${req.httpVersion} : ${req.method} : ${req.url} : New Request Received \n`;
-    console.log(myUrl);
     fs.appendFile("app.log", log, (err, result) => {
 
-        switch (myUrl.pathname) {
+        switch (parsedUrl.pathname) {
             case "/":
                 res.end("Home Page");
                 break;
             case "/about":
-                const username = myUrl.query?.name || "Ashish";
+                const username = parsedUrl.query?.name || "Ashish";
                 res.end(`My Name is ${username}`);
                 break;
             default:
