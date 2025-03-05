15e3c96f859b7f2492b3e7e4f767e8537316eca2
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-03-05 23:08:46 +0530

    implemented crud for users

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const port = 8000;
 
+const users = require("./mock.json");
+
 app.get("/", (req, res) => {
     res.send("Hello from Express Homepage")
 })
@@ -13,6 +15,39 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.send(`Hello ${req.query?.name || "Nakul"} , your age is ${req.query.age || 21}`)
 })
+
+app.get("/users", (req, res) => {
+    const html = `<ul>${users.map(user => `<li>${user.id} - ${user.first_name} ${user.last_name} ${user.email}</li>`).join("")}</ul>`
+    res.send(html)
+})
+
+app.get("/api/users", (req, res) => {
+    res.json(users);
+})
+
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id)
+    res.json(users.find(user => user.id === id))
+})
+
+app.post("/api/users", (req, res) => {
+    console.log(req.body)
+    res.json("pending")
+})
+
+app.patch("/api/users/:id", (req, res) => {
+    console.log(req.body)
+    res.json("pending")
+})
+
+app.delete("/api/users/:id", (req, res) => {
+    console.log(req.body)
+    res.json("pending")
+})
+
+
+
+
 
 app.listen(port, () => {
     console.log("Server is running on port - " + port);
